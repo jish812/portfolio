a6ce6c91ff4d06d63622864d22509bb19b64e29b
jishnu <jishnusurajila@gmail.com>
2020-05-10 00:02:56 +0530

    added home

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import React from "react";
-import "./App.css";
-import { Navigation, Drawer, Content, Layout, Header } from "react-mdl";
-import Main from "./Components/Main";
-import { Link } from "react-router-dom";
+import Home from "./Components/Home";
 
 export default function App() {
   return (
-    <React.Fragment>
-      <div className="demo-big-content">
-        <Layout>
-          <Header className="header-color" title="my page" scroll>
-            <Navigation>
-              <Link className="top-nav" to="/resume">
-                Resume
-              </Link>
-              <Link className="top-nav" to="/about">
-                About Me
-              </Link>
-              <Link className="top-nav" to="/project">
-                Project
-              </Link>
-              <Link className="top-nav" to="/contact">
-                Contact
-              </Link>
-            </Navigation>
-          </Header>
-          <Drawer title="Title">
-            <Navigation>
-              <Link to="/resume">Resume</Link>
-              <Link to="/about">About Me</Link>
-              <Link to="/project">Project</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>{" "}
-    </React.Fragment>
+    <div>
+      <Home />
+    </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Assets/Css/Home.css";
+import { Navigation, Drawer, Content, Layout, Header } from "react-mdl";
+import Main from "./Main";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title="my page" scroll>
+            <Navigation>
+              <Link className="top-nav" to="/resume">
+                Resume
+              </Link>
+              <Link className="top-nav" to="/about">
+                About Me
+              </Link>
+              <Link className="top-nav" to="/project">
+                Project
+              </Link>
+              <Link className="top-nav" to="/contact">
+                Contact
+              </Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about">About Me</Link>
+              <Link to="/project">Project</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>{" "}
+    </React.Fragment>
+  );
+}
